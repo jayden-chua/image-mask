b3bd51befb22f4b85d88ca24152094585fdc108e
Jayden Chua <jayden.chua@modix.cn>
2018-04-28 17:43:45 +0800

    add upgrade pip command

--- setup_env.sh
+++ setup_env.sh
@@ -19,6 +19,7 @@ fi
 conda create -n py35_albu python=3.5
 source activate py35_albu
 
+pip install --upgrade pip
 pip install -r ./albu/requirements.txt
 pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl
 pip install torchvision
@@ -28,6 +29,8 @@ source deactivate
 
 conda create -n py35_ternaus python=3.5
 source activate py35_ternaus
+
+pip install --upgrade pip
 pip install -r ./ternaus/requirements.txt
 pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl
 pip install torchvision
@@ -38,6 +41,7 @@ source deactivate
 conda create -n py27 python=2.7
 source activate py27
 
+pip install --upgrade pip
 pip install -r requirements.txt
 pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl
 pip install torchvision 
