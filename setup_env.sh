d4c6e8526f1e24bfc7ce46811075a855e36849ef
Jayden Chua <jayden.chua@modix.cn>
2018-07-08 22:41:55 +0800

    revert setup to only allow linux

--- setup_env.sh
+++ setup_env.sh
@@ -1,40 +1,38 @@
 #!/usr/bin/env bash
 
-unamestr=`uname`
-
 which conda
 if [ "$?" -eq 1 ]; then
-    if [[ "$unamestr" == 'Linux' ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O ~/miniconda.sh
-    elif [[ "$unamestr" == 'Darwin' ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
-    fi
-
-    bash ~/miniconda.sh -b -p $HOME/miniconda
-    echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc && source ~/.bashrc
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+    chmod +x Miniconda2-latest-Linux-x86_64.sh
+    ./Miniconda2-latest-Linux-x86_64.sh
 else
     echo "Miniconda already installed"
 fi
 
-conda create -n py35_all python=3.5
-source activate py35_all
+conda create -n py35_albu python=3.5
+source activate py35_albu
 
-pip install --upgrade pip
 pip install -r ./albu/requirements.txt
 pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl
 pip install torchvision
-pip install jupyter
 
 source deactivate
 
 
-#conda create -n py27 python=2.7
-#source activate py27
+conda create -n py35_ternaus python=3.5
+source activate py35_ternaus
+pip install -r ./ternaus/requirements.txt
+pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp35-cp35m-manylinux1_x86_64.whl
+pip install torchvision
 
-#pip install --upgrade pip
-#pip install -r requirements.txt
-#pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl
-#pip install torchvision
+source deactivate
 
-#source deactivate
 
+conda create -n py27 python=2.7
+source activate py27
+
+pip install -r requirements.txt
+pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl
+pip install torchvision 
+
+source deactivate
