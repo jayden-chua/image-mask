0200c10be3e5cad604d6a8175eb3364c92cb129c
Jayden Chua <jayden.chua@modix.cn>
2020-08-03 08:57:04 +0800

    add test features

---
+++ basic/decode.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+def rle_decode(mask_rle, shape):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
+
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape)
+
+
+masks = pd.read_csv('../output/submit.csv')
+num_masks = masks.shape[0]
+
+for r in masks.itertuples():
+    mask = rle_decode(r.rle_mask, (388, 388))
+    print(mask)
